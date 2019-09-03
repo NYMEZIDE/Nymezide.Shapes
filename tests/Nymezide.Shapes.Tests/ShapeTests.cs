4022c59b2cddfad50fcb9c964d6e6112bfae1f04
Ivan Filatov <nymezide@gmail.com>
2019-09-03 15:15:23 +0500

    add ShapeBuilder test via Func

--- tests/Nymezide.Shapes.Tests/ShapeTests.cs
+++ tests/Nymezide.Shapes.Tests/ShapeTests.cs
@@ -23,34 +23,47 @@ namespace Nymezide.Shapes.Tests
             serviceProvider = services.BuildServiceProvider();
         }
 
-        [Theory(DisplayName = "Success_Square_Shape_via_Triangle")]
+        [Theory(DisplayName = "Success_Square_Shape_by_Triangle")]
         [InlineData(5, 5, 5)]
         [InlineData(10, 10, 10)]
         [InlineData(20, 20, 20)]
-        public async Task SuccessSquareShapeViaTriangle(double a, double b, double c)
+        public async Task SuccessSquareShapeByTriangle(double a, double b, double c)
         {
             var builder = serviceProvider.GetService<IShapeBuilder>();
-            Shape shape = await builder.ProcessAsync(new ThreeSidesOptions(a, b, c));
 
+            Shape shape = await builder.ProcessAsync(new ThreeSidesOptions(a, b, c));
             Exception ex = Record.Exception(() => shape.GetSquare());
 
             Assert.Null(ex);
             Assert.NotEqual(0, shape.GetSquare());
         }
 
-        [Theory(DisplayName = "Success_Square_Shape_via_Circle")]
+        [Theory(DisplayName = "Success_Square_Shape_by_Circle")]
         [InlineData(5)]
         [InlineData(10)]
         [InlineData(20)]
-        public async Task SuccessSquareShapeViaCircle(double radius)
+        public async Task SuccessSquareShapeByCircle(double radius)
         {
             var builder = serviceProvider.GetService<IShapeBuilder>();
-            Shape shape = await builder.ProcessAsync(new RadiusOptions(radius));
 
+            Shape shape = await builder.ProcessAsync(new RadiusOptions(radius));
             Exception ex = Record.Exception(() => shape.GetSquare());
 
             Assert.Null(ex);
             Assert.NotEqual(0, shape.GetSquare());
+        }
+
+        [Theory(DisplayName = "Success_Square_Shape_via_Func")]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(20)]
+        public async Task SuccessSquareShapeViaFunc(double radius)
+        {
+            var builder = serviceProvider.GetService<IShapeBuilder>();
+
+            Shape shape = await builder.ProcessAsync(() => new RadiusOptions(radius));
+
+            Assert.NotNull(shape);
         }
     }
 }
